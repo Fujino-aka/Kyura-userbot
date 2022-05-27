ce41be7d67c8ea8182a7cc9c9c728560a1fe722d
Aka <95154437+Fujino-aka@users.noreply.github.com>
2022-05-27 10:54:11 +0530

    Create startup.py

---
+++ userbot/utils/startup.py
@@ -0,0 +1,82 @@
+import glob
+import os
+import sys
+import urllib.request
+from datetime import timedelta
+from pathlib import Path
+
+from telethon import Button, functions, types, utils
+
+from ..Config import Config
+from ..core.logger import logging
+from ..core.session import catub
+from .pluginmanager import load_module
+
+ENV = bool(os.environ.get("ENV", False))
+LOGS = logging.getLogger("CatUBStartUP")
+cmdhr = Config.COMMAND_HAND_LER
+
+if ENV:
+    VPS_NOLOAD = ["vps"]
+elif os.path.exists("config.py"):
+    VPS_NOLOAD = ["heroku"]
+
+
+async def load_plugins(folder, extfolder=None):
+    """
+    To load plugins from the mentioned folder
+    """
+    if extfolder:
+        path = f"{extfolder}/*.py"
+        plugin_path = extfolder
+    else:
+        path = f"userbot/{folder}/*.py"
+        plugin_path = f"userbot/{folder}"
+    files = glob.glob(path)
+    files.sort()
+    success = 0
+    failure = []
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            pluginname = shortname.replace(".py", "")
+            try:
+                if (pluginname not in Config.NO_LOAD) and (
+                    pluginname not in VPS_NOLOAD
+                ):
+                    flag = True
+                    check = 0
+                    while flag:
+                        try:
+                            load_module(
+                                pluginname,
+                                plugin_path=plugin_path,
+                            )
+                            if shortname in failure:
+                                failure.remove(shortname)
+                            success += 1
+                            break
+                        except ModuleNotFoundError as e:
+                            install_pip(e.name)
+                            check += 1
+                            if shortname not in failure:
+                                failure.append(shortname)
+                            if check > 5:
+                                break
+                else:
+                    os.remove(Path(f"{plugin_path}/{shortname}.py"))
+            except Exception as e:
+                if shortname not in failure:
+                    failure.append(shortname)
+                os.remove(Path(f"{plugin_path}/{shortname}.py"))
+                LOGS.info(
+                    f"unable to load {shortname} because of error {e}\nBase Folder {plugin_path}"
+                )
+    if extfolder:
+        if not failure:
+            failure.append("None")
+        await catub.tgbot.send_message(
+            BOTLOG_CHATID,
+            f'Your external repo plugins have imported \n**No of imported plugins :** `{success}`\n**Failed plugins to import :** `{", ".join(failure)}`',
+        )
